2fa792c17ca8a384ee241bc5f9b26a4f8c9f4fea
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-08 15:44:07 -0600

    getAll model function and endpoint written

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,5 +1,31 @@
-const express = require('express');
-const db = require('../../data/db-config');
-const router = express.Router();
+const router = require('express').Router();
+const Car = require('./cars-model');
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require('../cars/cars-middleware');
+
+router.get('/', (req, res, next) => {
+  Car.getAll()
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {});
+
+router.post('/', (req, res, next) => {});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    custom: 'something went wrong with your request',
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = router;
