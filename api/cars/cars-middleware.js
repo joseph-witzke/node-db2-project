8c823106e9c200f3717ce8c850d9ef8fb0dcc703
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-08 15:29:08 -0600

    checkCarId middleware written

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,35 @@
+const Car = require('../cars/cars-model');
+const db = require('../../data/db-config');
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  Car.getById(req.params.id)
+    .then((car) => {
+      if (!car) {
+        res.status(404).json({ message: `car with id ${id} is not found` });
+      } else {
+        req.car = car;
+        next();
+      }
+    })
+    .catch(next);
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
